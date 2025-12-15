b42329444d735c1bb400aa986e69cfae4289d38c
bence <bence.matuz1@gmail.com>
2025-12-15 22:01:06 +0100

    add empty service classes and PLAN.md

---
+++ PLAN.md
@@ -0,0 +1,5 @@
+Service logika implementálása
+Controller-ben történő használat
+(Postman-nel tesztelés)
+Angular frontend framework
+CSS framework?
---
+++ src/main/java/com/example/demo/controller/BowlController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Bowl;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/bowls")
+public class BowlController {
+    @GetMapping
+    public List<Bowl> getAllBowl() {
+        return null;
+    }
+}
